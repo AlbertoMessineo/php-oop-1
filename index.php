c381e5b6131b58d65198dfb7544a78ac630adac4
AlbertoMessineo <albevorox@gmail.com>
2024-01-15 17:53:15 +0100

    feat : movie with construct method

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/models/Movie.php';
 
 // require_once __DIR__ . '/models/Address.php';
 // require_once __DIR__ . '/models/Helper.php';
-echo 60 . " min";
+// echo 60 . " min";
 
-$movie_1 = new movie ("Redivivo", "Avventura", "110", "qualcosa");
+$movie_1 = new movie ("The Revenant", "Avventura", "10", "USA");
 
 var_dump($movie_1);
--- models/Movie.php
+++ models/Movie.php
@@ -7,4 +7,25 @@ class Movie
   public $duration;
   public $origin;
 
+
+  public function __construct($_title , $_genre, $_duration, $_origin )
+  {
+    $this->title = $_title;
+    $this->genre = $_genre;
+    $this->duration = $_duration;
+    $this->origin = $_origin;
+    
+  }
+
+  public function DetDuration($_duration)
+  //funzione che verifica la durata corrisponde a quella di un film, espressa in Minuti
+  {
+
+    if ($_duration >= 60) {
+        $this->duration = $_duration;
+    }
+    else{
+        echo " This is not a Movie, this is a Short Film";
+    }
+  }
 }
