8381dd4fb703d4f2eaaa7cc058e9e0aadd756d82
AlbertoMessineo <albevorox@gmail.com>
2024-01-15 17:56:39 +0100

    feat: second movie printed

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ require_once __DIR__ . '/models/Movie.php';
 // require_once __DIR__ . '/models/Helper.php';
 // echo 60 . " min";
 
-$movie_1 = new movie ("The Revenant", "Avventura", "10", "USA");
+$movie_1 = new movie ("The Revenant", "Western, Avventura", "157", "USA");
+$movie_2 = new movie ("La dolce vita", "Drammatico", "154", "Italia");
 
 var_dump($movie_1);
+var_dump($movie_2);
