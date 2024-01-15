d726b5763558d3ea2f540baa14daf4e5407ca86e
AlbertoMessineo <albevorox@gmail.com>
2024-01-15 18:00:48 +0100

    feat: es ended no bonuses

--- index.php
+++ index.php
@@ -10,5 +10,21 @@ require_once __DIR__ . '/models/Movie.php';
 $movie_1 = new movie ("The Revenant", "Western, Avventura", "157", "USA");
 $movie_2 = new movie ("La dolce vita", "Drammatico", "154", "Italia");
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php   
 var_dump($movie_1);
+?>
+<hr>
+<?php   
 var_dump($movie_2);
+?>
+</body>
+</html>
